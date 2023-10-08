14b087b7d3b8bfe8bd36e924f8883a1d752fa981
nisargptl <nisarg3000@gmail.com>
2023-10-08 13:00:29 -0700

    final commit

--- BE_DRM/Uploader/settings.py
+++ BE_DRM/Uploader/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -111,6 +111,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+# this is directory name where collectstatic files command will put your app level static files
+STATIC_ROOT = 'staticfiles'
+
+# this is directory paths where you have to put your project level static files
+# you can put multiple folders here
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- BE_DRM/Uploader/views.py
+++ BE_DRM/Uploader/views.py
@@ -1,4 +1,7 @@
 import boto3
+from PIL import Image
+import pytesseract
+import numpy as np
 from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -13,37 +16,75 @@ pwd = os.path.dirname(__file__)
 import moviepy.editor as mp
 import speech_recognition as sr
 
+# def getImageKeywords(file):
+#     keywords = []
+#     pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+#     img1 = np.array(Image.open(file))
+#     text = pytesseract.image_to_string(img1)
+#     total_words = text.split()
+#     total_word_length = len(total_words)
 
-def getAudioKeywords(file):
-    import os
-    video_file = file
-    video_clip = mp.VideoFileClip(video_file)
-    audio = video_clip.audio
-    temp_audio_file = "temp_audio.wav"
-    audio.write_audiofile(temp_audio_file)
-    recognizer = sr.Recognizer()
-    recognizer.energy_threshold = 4000  
-    with sr.AudioFile(temp_audio_file) as source:
-        audio_data = recognizer.record(source)
-        try:
-            text = recognizer.recognize_sphinx(audio_data)
-            print("Extracted Text: " + text)
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-        except sr.RequestError as e:
-            print(f"Recognition error; {e}")
+#     language = "en"
+#     numOfKeywords = int(total_word_length*0.15)
+#     text_keywords = []
+#     kw_extractor = yake.KeywordExtractor(stopwords = stop_words, n=1, top = numOfKeywords )
+#     keywords = kw_extractor.extract_keywords(text)
+#     for kw in keywords:
+#         text_keywords.append(kw[0])
 
-    os.remove(temp_audio_file)
+#     # path = "files"
+#     # dir_list = os.listdir(path)
+#     # # print(len(dir_list))
+#     # for file in dir_list:
+#     #     filename = file.split(".")
+#     filename = file.split(".")
+#     image_metadata = {}
+#     image_metadata['text_keywords'] = text_keywords
+#     image_metadata['filename'] = filename
+#     if(filename.pop() == 'jpg'):
+#         image = Image.open(file)
+#         exifdata = image.getexif()
+#         for tag_id in exifdata:
+#             tag = TAGS.get(tag_id, tag_id)
+#             data = exifdata.get(tag_id)
+#             if isinstance(data, bytes):
+#                 data = data.decode()
+#             if(tag == 'DateTime'):
+#                 image_metadata['DateTime'] = data
+#             if(tag=='ImageDescription'):
+#                 image_metadata['description'] = data
+#     return image_metadata
 
-    audio_keywords = []
-    total_words = text.split()
-    total_word_length = len(total_words)
-    numOfKeywords = int(total_word_length*0.15)
-    kw_extractor = yake.KeywordExtractor(stopwords = stop_words, n=1, top = numOfKeywords)
-    keywords = kw_extractor.extract_keywords(text)
-    for kw in keywords:
-        audio_keywords.append(kw)
-    return audio_keywords
+# def getAudioKeywords(file):
+#     import os
+#     video_file = file
+#     video_clip = mp.VideoFileClip(video_file)
+#     audio = video_clip.audio
+#     temp_audio_file = "temp_audio.wav"
+#     audio.write_audiofile(temp_audio_file)
+#     recognizer = sr.Recognizer()
+#     recognizer.energy_threshold = 4000  
+#     with sr.AudioFile(temp_audio_file) as source:
+#         audio_data = recognizer.record(source)
+#         try:
+#             text = recognizer.recognize_sphinx(audio_data)
+#             print("Extracted Text: " + text)
+#         except sr.UnknownValueError:
+#             print("Could not understand audio")
+#         except sr.RequestError as e:
+#             print(f"Recognition error; {e}")
+
+#     os.remove(temp_audio_file)
+
+#     audio_keywords = []
+#     total_words = text.split()
+#     total_word_length = len(total_words)
+#     numOfKeywords = int(total_word_length*0.15)
+#     kw_extractor = yake.KeywordExtractor(stopwords = stop_words, n=1, top = numOfKeywords)
+#     keywords = kw_extractor.extract_keywords(text)
+#     for kw in keywords:
+#         audio_keywords.append(kw)
+#     return audio_keywords
 
 
 def getDocKeywords(file):
@@ -75,11 +116,15 @@ def upload_to_s3(request):
         if extension == 'doc':
             docKeywords = getDocKeywords(file_name)
             keyWords += docKeywords
-        elif extension == 'mp4':
-            with open(file_name, 'wb+') as f:
-                for chunk in file.chunks():
-                    f.write(chunk)
-            # audioKeywords = getAudioKeywords(file_name)
+        # elif extension == 'jpg':
+            # imageKeywords = getImageKeywords(file_name)
+            # print(imageKeywords)
+            # keywords += imageKeywords
+        # elif extension == 'mp4':
+        #     with open(file_name, 'wb+') as f:
+        #         for chunk in file.chunks():
+        #             f.write(chunk)
+        # audioKeywords = getAudioKeywords(file_name)
         
 
         s3 = boto3.client(
